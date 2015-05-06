5716eb3a5c378ab2b4e28dc1bdea28c3049ea29c
EleanorStrotz <EleanorStrotz@gmail.com>
2015-05-06 11:25:29 -0700

    almost done with milestone 2

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
+<?php
+//Configuration for our PHP Server
+set_time_limit(0);
+ini_set('default_socket_timeout', newvalue)
+
+
+
+?>
+
+
 CLIENT ID	2181409914a14faeb598ef20216bc346
 CLIENT SECRET	d9a01b57658c4a1981efc3e91ae1a148
 WEBSITE URL	https://localhost/eleanorstrotz
 REDIRECT URI	https://localhost/eleanorstrotz
+
+<!-- 2:36 video 2 -->
