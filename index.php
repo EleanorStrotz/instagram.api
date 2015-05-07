68adf367487c485633aef360f659e35597f0de9a
EleanorStrotz <EleanorStrotz@gmail.com>
2015-05-07 11:04:33 -0700

    video 2

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php
 //Configuration for our PHP Server
 set_time_limit(0);
-ini_set('default_socket_timeout', newvalue)
+ini_set('default_socket_timeout', 300);
+session_start();
+
+//Make Constants using define.
+define('client_id', '2181409914a14faeb598ef20216bc346');
+define('client_secret', 'd9a01b57658c4a1981efc3e91ae1a148');
+define('redirectURI', 'https://localhost/eleanorstrotz');
+define('ImageDirectory', 'pics/');
 
 
 
 ?>
 
 
-CLIENT ID	2181409914a14faeb598ef20216bc346
+<!-- CLIENT ID	2181409914a14faeb598ef20216bc346
 CLIENT SECRET	d9a01b57658c4a1981efc3e91ae1a148
 WEBSITE URL	https://localhost/eleanorstrotz
 REDIRECT URI	https://localhost/eleanorstrotz
-
+ -->
 <!-- 2:36 video 2 -->
