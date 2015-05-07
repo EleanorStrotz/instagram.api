eb3f66b99d8900237a8459c935d0aa7e013331b5
EleanorStrotz <EleanorStrotz@gmail.com>
2015-05-07 11:25:24 -0700

    almost done with video 3

--- index.php
+++ index.php
@@ -5,19 +5,28 @@ ini_set('default_socket_timeout', 300);
 session_start();
 
 //Make Constants using define.
-define('client_id', '2181409914a14faeb598ef20216bc346');
-define('client_secret', 'd9a01b57658c4a1981efc3e91ae1a148');
+define('client_ID', '2181409914a14faeb598ef20216bc346');
+define('client_Secret', 'd9a01b57658c4a1981efc3e91ae1a148');
 define('redirectURI', 'https://localhost/eleanorstrotz');
 define('ImageDirectory', 'pics/');
 
-
-
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<!-- CLIENT ID	2181409914a14faeb598ef20216bc346
-CLIENT SECRET	d9a01b57658c4a1981efc3e91ae1a148
-WEBSITE URL	https://localhost/eleanorstrotz
-REDIRECT URI	https://localhost/eleanorstrotz
- -->
-<!-- 2:36 video 2 -->
+	<title>Untitled</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="author" href="humans.txt">
+</head>
+<body>
+	<a href="https:api.instagram/oauth/authorize/?client_id=xxx">LOGIN</a>
+	<script type="text/javascript"></script>
+</body>
+</html>
+
+<!-- video 3 4:53 -->
